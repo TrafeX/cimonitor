d5c6f2c6f9f85851874e9de59db2e33f183989f0
Tim de Pater <git@trafex.nl>
2016-03-21 15:45:34 +0100

    Fixed overflow issue on older browsers

--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-rsync -avh --exclude=.git --exclude=.idea --exclude=node_modules . cimonitor.office.hq.enrise.net:cimonitor/
+rsync -avh --exclude=.git --exclude=.vagrant --exclude=.idea --exclude=node_modules . cimonitor.office.hq.enrise.net:cimonitor/
